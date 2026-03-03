b9e220c4d1d77f12a837c8dac9006696bdc3e310
fhanif16 <fatemahanif16@gmail.com>
2026-03-03 18:54:13 -0500

    final

--- src/components/ticketCard/TicketCards.jsx
+++ src/components/ticketCard/TicketCards.jsx
@@ -11,7 +11,7 @@ export default function TicketCards({tickets , handleTask , task , handleResolve
     <div className='grid grid-cols-12 gap-4'>
 
         <div className="col-span-12 lg:col-span-9">
-            <h1 className='text-2xl ml-6 mt-6'>Customer Tickets</h1>
+            <h1 className='text-2xl ml-6 mt-6 font-bold'>Customer Tickets</h1>
            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-6">
             
 
@@ -29,12 +29,12 @@ export default function TicketCards({tickets , handleTask , task , handleResolve
 
         <div className="col-span-12 lg:col-span-3">
             <div>
-                <h1 className='text-2xl ml-6 mt-6'>Task Status</h1>
+                <h1 className='text-2xl ml-6 mt-6 font-bold'>Task Status</h1>
             <TaskStatus task={task} handleResolved={handleResolved}></TaskStatus>
             </div>
             
            <div>
-            <h1 className='text-2xl ml-6 mt-6'>Resolved Task</h1>
+            <h1 className='text-2xl ml-6 mt-6 font-bold'>Resolved Task</h1>
             <TaskResolved  resolvedTask ={resolvedTask }></TaskResolved>
            </div>
              
