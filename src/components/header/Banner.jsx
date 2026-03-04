92a31014134cd5363f0dd522ae63fecb295930b3
fhanif16 <fatemahanif16@gmail.com>
2026-03-03 22:37:34 -0500

    push

--- src/components/header/Banner.jsx
+++ src/components/header/Banner.jsx
@@ -7,7 +7,7 @@ export default function Banner({task , resolvedTask}) {
     <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2' >
 
 
-        <div className='bg-blue-500 h-[200px]' style={{backgroundImage:`url(${logo})`} } >
+        <div className=' h-[200px]' style={{ backgroundImage: `url(${logo}), linear-gradient(to right, #632EE3, #9F62F2)`} } >
           <div className='text-center m-16'>
             <h1>In-Progress</h1>
             <p>{task.length}</p>
@@ -23,7 +23,7 @@ export default function Banner({task , resolvedTask}) {
         </div>
 
 
-        <div className='bg-green-500 h-[200px]' style={{backgroundImage:`url(${logo})`} } >
+        <div className=' h-[200px]' style={{ backgroundImage: `url(${logo}), linear-gradient(to right, #54CF68, #00827A)`} } >
           <div className='text-center m-16'>
             <h1>Resolved</h1>
             <p>{resolvedTask.length}</p>
